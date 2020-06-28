418fedf2aa4d487abdecc9565d3f0184d670a87a
Miriam <miriam.haenle@gmail.com>
2020-06-28 15:41:59 +0200

    Added axios and made request to external api

--- functions/index.js
+++ functions/index.js
@@ -1,80 +1,25 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express');
-
+const axios = require('axios');
+const cors = require('cors');
+const { request } = require('http');
 const app = express();
+
+app.use(cors({ origin: true }));
 app.use(express.json());
 
 admin.initializeApp();
-
-app.get('/', (request, response) => {
-  response.send('Hello world!');
+app.get('/hello-world', (request, response) => {
+  return response.status(200).send('Hello world!');
 });
 
-app.post('/', (request, response) => {
-  const testCollection = admin.firestore().collection('test');
-  let message = 'No entry was added';
-});
+exports.app = functions.region('europe-west3').https.onRequest(app);
 
-exports.api = functions.region('europe-west3').https.onRequest(app);
-
-/*
-exports.helloWorld = functions
-  .region('europe-west3')
-  .https.onRequest((request, response) => {
-    response.send('Hello from the other side !');
-  });
-
-exports.add = functions
-  .region('europe-west3')
-  .https.onRequest(async (request, response) => {
-    const testCollection = admin.firestore().collection('test');
-    let message = 'No entry was added';
-
-    if (request.query.name && request.query.age) {
-      await testCollection.add({
-        name: request.query.name,
-        age: request.query.age,
-      });
-
-      message = 'Added one entry to test collection';
-    }
-
-    return response.json({
-      message,
-    });
-  });
-
-exports.getAll = functions
-  .region('europe-west3')
-  .https.onRequest(async (request, response) => {
-    const testCollection = admin.firestore().collection('test');
-
-    const snapshot = await testCollection.get();
-    let data = [];
-    snapshot.forEach((doc) => data.push(doc.data()));
-
-    return response.json({
-      message: 'Added one entry to test collection',
-      data,
-    });
-  });
-
-exports.getTeenager = functions
-  .region('europe-west3')
-  .https.onRequest(async (request, response) => {
-    const testCollection = admin.firestore().collection('test');
-    const query = await testCollection
-      .where('age', '<', '20')
-      .select('name', 'age');
-    const snapshot = await query.get();
-
-    let data = [];
-    snapshot.forEach((doc) => data.push(doc.data()));
-
-    return response.json({
-      message: 'Added one entry to test collection',
-      data,
-    });
-  });
-*/
+axios
+  .get(
+    'https://api.triptocarbon.xyz/v1/footprint?activity=186&activityType=miles&country=def&mode=dieselCar'
+  )
+  .then((response) => response.data)
+  .then((footprint) => console.log(footprint))
+  .catch((error) => console.log(error));
